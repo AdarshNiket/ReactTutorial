594beb7df7695bd5fe52372a99b132a619e814ca
Niket <212595088@logon.ds.ge.com>
2020-03-12 14:15:14 +0530

    added index.html,index.js and configured webpack for development

---
+++ webpack.config.dev.js
@@ -0,0 +1,48 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// notifies babel plugin that we are running in dev mode.
+process.env.NODE_ENV = "development";
+
+//webpack configure settings
+module.exports = {
+  mode: "development",
+  target: "web",
+  devTool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  //   to serve app in dev, webpack settings
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFalback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugin: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  //   tell webpack what files to handle
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
