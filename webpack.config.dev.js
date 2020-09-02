100b3191ba21a27385540af1ca36666e31b07ab9
AdarshNiket <adarsh.niket@gmail.com>
2020-09-02 12:22:09 +0530

    wip code pushed related to babel, webpack, etc

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,8 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-// notifies babel plugin that we are running in dev mode.
 process.env.NODE_ENV = "development";
 
-//webpack configure settings
 module.exports = {
   mode: "development",
   target: "web",
@@ -14,35 +12,33 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
-    filename: "bundle.js",
+    filename: "bundle.js"
   },
-  //   to serve app in dev, webpack settings
   devServer: {
     stats: "minimal",
     overlay: true,
-    historyApiFalback: true,
+    historyApiFallback: true,
     disableHostCheck: true,
     headers: { "Access-Control-Allow-Origin": "*" },
-    https: false,
+    https: false
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
-      favicon: "src/favicon.ico",
-    }),
+      favicon: "src/favicon.ico"
+    })
   ],
-  //   tell webpack what files to handle
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ["babel-loader"]
       },
       {
         test: /(\.css)$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
