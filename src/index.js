4abf23e47b4e01dad9e3977533976dcc6cba8edc
AdarshNiket <adarsh@buddhimed.com>
2020-11-24 15:48:50 +0530

    changes in webpack config and package.json file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { ReactDOM, render } from "react-dom";
 
-// function Hi() {
-//   return <p>Hi.</p>;
-// }
+function Hi() {
+  return <p>Hi</p>;
 
 ReactDOM.render(<p>Hi.</p>, document.getElementById("app"));
